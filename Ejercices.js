fe581c469daad7379e9e580e452fdd931ff20d17
Andy c <81699558+andycr123@users.noreply.github.com>
2024-07-15 19:37:20 -0500

    Add files via upload

---
+++ Ejercices.js
@@ -0,0 +1,32 @@
+//  Ejercicios básicos:
+
+// 1. **Ejercicio de Variables y Operaciones:**
+// Declara una variable 'edad' y pidele la edad al usuario
+/* Respuestas: */
+// let edad;
+// edad = prompt("Por favor, ingresa tu edad:");
+// console.log("La edad ingresada es: " + edad);
+// alert("La edad ingresada es: " + edad);
+
+
+// 2. **Ejercicio de Funciones:**
+ // Define una función llamada 'sumar' que reciba dos parámetros 'a' y 'b' y devuelva la suma de ambos.
+ /* Respuestas */
+
+// 3. **Ejercicio de Arrays:**
+// Crea un array llamado 'colores' que contenga tres nombres de colores como strings y filtra sobre ellos
+/* Respuesta : */
+
+// function sumar(a, b) {
+//     return a + b;
+// }
+// let resultado = sumar(5, 3);
+// console.log("El resultado de la suma es: " + resultado);
+
+
+// 4. **Ejercicio de Control de Flujo:**
+// Escribe un bucle 'for' que imprima los números del 1 al 5 en la consola.   ```
+/* Respuestas: */
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
